5f0dc8689fcc833e938476d0178171853a8d567c
emilio macharia <132134982+MachariaEmilio@users.noreply.github.com>
2024-06-24 09:05:31 +0300

    Delete 2nd exercise directory

--- 2nd exercise/router/routes.mjs
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { get, getAll, gete } from "../controllers/controller.mjs";
-
-const router = Router()
-
-
-router.route("/").get(gete)
-
-export default router
